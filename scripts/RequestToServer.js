acd172d689fc923fdf079d554eba22a5c72d6188
unknown <jwong3@wpi.edu>
2018-11-03 14:25:59 -0400

    Added some error checking

--- scripts/RequestToServer.js
+++ scripts/RequestToServer.js
@@ -32,8 +32,11 @@ getRequest.onload = function() {
 
 getItemRequest.onload = function() {
     if (this.readyState === 4 && this.status === 200) {
-        openEditEditor();
-        populateEditor(JSON.parse(this.responseText));
+        var data = JSON.parse(this.responseText);
+        if (data.length > 0) {
+            openEditEditor();
+            populateEditor(data);
+        }
     }
 };
 
